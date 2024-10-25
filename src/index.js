2a4feb98d80c8b2c8c57685425a64ae5ad763a90
Filip Rakowski <f.rakowskI@hotmail.com>
2024-10-25 16:49:53 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function getFiles(directory) {
+
+}
+
+function readFile(filePath) {
+
+}
+
+function writeFile(filePath, content) {
+
+}
+
+function modifyFile(filePath, prompt) {
+
+}
+
+function main() {
+
+}
