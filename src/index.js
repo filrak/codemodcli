5493c8da77e96aa37980d643b8e645f177731b57
Filip <f.rakowski@hotmail.com>
2024-10-28 12:40:13 +0100

    differentiate frameworks

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 const fs = require('fs');
-const path = require('path');
 const { useChat } = require('./chat');
-
-// we can use this if we want to modify the whole repository
-function getFileList(directory) {
-  const result = {};
-
-  function traverseDirectory(currentPath) {
-    const files = fs.readdirSync(currentPath);
-
-    for (const file of files) {
-      const filePath = path.join(currentPath, file);
-      const stats = fs.statSync(filePath);
-
-      if (stats.isDirectory()) {
-        traverseDirectory(filePath);
-      } else {
-        const relativePath = path.relative(directory, filePath);
-        const content = fs.readFileSync(filePath, 'utf-8');
-        result[relativePath] = content;
-      }
-    }
-  }
-
-  traverseDirectory(directory);
-  return result;
-}
 
 async function getFileContent(fileList) {
   const fileContent = {};
@@ -54,14 +28,19 @@ function writeFile(filePath, content) {
 }
 
 async function main() {
+    const frameworkInstructions = {
+      nuxt: 'Ignore tsx file edits and dont put them on a list.',
+      next: 'Ignore vue files edits and dont put them on a list.'
+    }
+    
     const currentDirectory = process.cwd();
     const instructions = readFile(currentDirectory + '/playground/instructions/01.md');
-    const { content: fileToEdit } = await useChat('Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas' + instructions);
-    const filesToModify = fileToEdit.split(',').map(file => currentDirectory + '/playground/storefront-unified-nuxt/' + file);
+    const { content: fileToEdit } = await useChat('Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas' + frameworkInstructions[nuxt]+ instructions);
+    const filesToModify = fileToEdit.split(',').map(file => currentDirectory + '/playground/storefront-unified-nuxt/' + file.replace(/\s/g, ''));
     const filesToModifyContent = await getFileContent(filesToModify);
     
     for (const [filePath, content] of Object.entries(filesToModifyContent)) {
-        const changedFile = await useChat('Modify the following file ' + content + ' according to the instructions in ' + instructions + ' and return only the modified file. Do not include any other text than the modified file, even the formatting for code blocks.');
+        const changedFile = await useChat('Modify the following file ' + content + ' according to the instructions in ' + instructions + ', and return only the modified file. Do not include any other text than the modified file, even the formatting for code blocks.');
         writeFile(filePath, changedFile.content);
     }
 }
