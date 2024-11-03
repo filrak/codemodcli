8d71aab4159a401ca8984a7074bdbc005ab089e2
Filip <f.rakowski@hotmail.com>
2024-11-03 15:52:01 +0100

    allow optional files to edit extratcion from instructions through LLM

--- src/files.js
+++ src/files.js
@@ -1,64 +1,62 @@
 const fs = require('fs');
 
 async function readFileList(fileList) {
-    const fileContent = {};
-    for (const filePath of fileList) {
-      try {
-        const content = fs.readFileSync(filePath, 'utf-8');
-        fileContent[filePath] = content;
-      } catch (error) {
-        console.error(`Error reading file ${filePath}:`, error);
-      }
-    }
-    return fileContent;
-  }
-  
-  function readFile(filePath) {
+  const fileContent = {};
+  for (const filePath of fileList) {
     try {
-      return fs.readFileSync(filePath, 'utf-8');
+      const content = fs.readFileSync(filePath, 'utf-8');
+      fileContent[filePath] = content;
     } catch (error) {
       console.error(`Error reading file ${filePath}:`, error);
-      return null;
     }
   }
-  
-  function writeFile(filePath, content) {
-      fs.writeFileSync(filePath, content);
-  }
+  return fileContent;
+}
 
-  function isFile(path) {
-    try {
-      const stats = fs.statSync(path);
-      return stats.isFile();
-    } catch (err) {
-      return false;
-    }
+async function readFile(filePath) {
+  try {
+    return fs.readFileSync(filePath, 'utf-8');
+  } catch (error) {
+    console.error(`Error reading file ${filePath}:`, error);
+    return null;
   }
+}
 
-  function readFiles(dirPath) {
-    const fileContent = {};
-    try {
-      const files = fs.readdirSync(dirPath);
-      for (const file of files) {
-        const filePath = `${dirPath}/${file}`;
-        if (isFile(filePath)) {
-          try {
-            fileContent[filePath] = fs.readFileSync(filePath, 'utf-8');
-          } catch (error) {
-            console.error(`Error reading file ${filePath}:`, error);
-          }
+async function writeFile(filePath, content) {
+    fs.writeFileSync(filePath, content);
+}
+async function isFile(path) {
+  try {
+    const stats = fs.statSync(path);
+    return stats.isFile();
+  } catch (err) {
+    return false;
+  }
+}
+async function readFiles(dirPath) {
+  const fileContent = {};
+  try {
+    const files = fs.readdirSync(dirPath);
+    for (const file of files) {
+      const filePath = `${dirPath}/${file}`;
+      if (isFile(filePath)) {
+        try {
+          fileContent[filePath] = fs.readFileSync(filePath, 'utf-8');
+        } catch (error) {
+          console.error(`Error reading file ${filePath}:`, error);
         }
       }
-    } catch (error) {
-      console.error(`Error reading directory ${dirPath}:`, error);
     }
-    return fileContent;
+  } catch (error) {
+    console.error(`Error reading directory ${dirPath}:`, error);
   }
-  
-  module.exports = { 
-    readFileList, 
-    readFiles, 
-    readFile,
-    writeFile,
-    isFile
+  return fileContent;
+}
+
+module.exports = { 
+  readFileList, 
+  readFiles, 
+  readFile,
+  writeFile,
+  isFile
 };
--- src/index.js
+++ src/index.js
@@ -14,13 +14,19 @@ async function run(options = { repository, instructionsDir, filesDir: '/', frame
     }
 
     const currentDirectory = process.cwd();
-    const instructions = isFile(currentDirectory + options.instructionsDir) 
-      ? readFile(currentDirectory + options.instructionsDir)
-      : readFiles(currentDirectory + options.instructionsDir);
-      // TO-DO: use extractFilesToEdit to conditionally extratc the list of files OR just run readFiles
-    const { content: fileToEdit } = await useChat('Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas' + frameworkInstructions[options.framework]+ instructions);
-    const filesToModify = fileToEdit.split(',').map(file => currentDirectory + options.filesDir + file.replace(/\s/g, ''));
-    const filesToModifyContent = await readFileList(filesToModify);
+    const instructions = await isFile(currentDirectory + options.instructionsDir) 
+      ? await readFile(currentDirectory + options.instructionsDir)
+      : await readFiles(currentDirectory + options.instructionsDir);
+      
+    let filesToModifyContent;
+
+    if (options.extractFilesToEdit) {
+      const { content: fileToEdit } = await useChat('Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas' + frameworkInstructions[options.framework]+ instructions);
+      const filesToModify = fileToEdit.split(',').map(file => currentDirectory + options.filesDir + file.replace(/\s/g, ''));
+       filesToModifyContent =  await readFileList(filesToModify);
+    } else {
+       filesToModifyContent =  await readFiles(currentDirectory + options.filesDir);
+    }
     
     for (const [filePath, content] of Object.entries(filesToModifyContent)) {
         const changedFile = await useChat('Modify the following file ' + content + ' according to the instructions in ' + instructions + ', and return only the modified file. Do not include any other text than the modified file, even the formatting for code blocks.');
