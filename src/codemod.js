5dd1ccd5c6f43e3646be74542b81cc4c17175130
Filip <f.rakowski@hotmail.com>
2024-11-04 20:43:04 +0100

    formatting adjustments

--- src/codemod.js
+++ src/codemod.js
@@ -26,7 +26,7 @@ async function runCodemod(options = { workingDir: '/' }) {
 
     if (options.instructions.extractFilesToEdit) {
       console.log(bold('Extracing list of files to edit from the instructions...'));
-      const { content: fileToEdit } = await useChat('Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas' + frameworkInstructions[options.framework]+ instructions);
+      const { content: fileToEdit } = await useChat(`Read the following instructions and extract the list of files that needs to be changed. Answer only with a list of files separated by commas${frameworkInstructions[options.framework]}${instructions}`);
       const filesToModify = fileToEdit.split(',').map(file => currentDirectory + options.workingDir + file.replace(/\s/g, ''));
       filesToModifyContent = readFileList(filesToModify);
       console.log(bold().green('Done!'));
@@ -36,7 +36,7 @@ async function runCodemod(options = { workingDir: '/' }) {
     console.log(bold('Modifying the files...'));
 
     for (const [filePath, content] of Object.entries(filesToModifyContent)) {
-        const changedFile = await useChat('You are a developer with a task of modifying a list of files following specific instructions. Modify the following file ' + content + ' according to the instructions in ' + instructions + ', and return only the modified file. Do not include any other text than the modified file, even the formatting for code blocks.');
+        const changedFile = await useChat(`You are a developer with a task of modifying a list of files following specific instructions. Modify the following file ${content} according to the instructions in ${instructions}, and return only the modified file. Do not include any other text than the modified file, even the formatting for code blocks.`);
         writeFile(filePath, changedFile.content);
         console.log(green().bold('Modified file: ') + filePath);
     }
