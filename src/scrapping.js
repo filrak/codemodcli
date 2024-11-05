06c735bb6e8f3a1d2ddb26ad16d9065cd2bd0231
Filip <f.rakowski@hotmail.com>
2024-11-05 13:15:56 +0100

    added scrapping [not connected to workflow yet]

---
+++ src/scrapping.js
@@ -0,0 +1,19 @@
+const cheerio = require('cheerio');
+
+async function scrapUrl(url, querySelector) {
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    
+    const extractedContent = $(querySelector).text();
+    return extractedContent.trim();
+  } catch (error) {
+    console.error('Error scraping URL:', error);
+    return null;
+  }
+}
+
+module.exports = {
+  scrapUrl
+};
